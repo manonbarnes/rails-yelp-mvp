eb190c79ea3435dd14a5d18d6193939fde6bc638
Manon Barnes <manonbarnes@icloud.com>
2021-03-17 10:53:16 +0100

    Changed type of restaurant phone number input

---
+++ app/models/restaurant.rb
@@ -0,0 +1,4 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'], message: 'in list' }
+end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, inclusion: { in: 0..5, message: " should be between 0 to 5" }
+  validates :rating, presence: { message: " cannot be blank" }
+  validates :rating, numericality: { message: " should be a number" }
+end
